efeea766f0a7e0e6de6fa5fcccbe2f84e1a961fb
0xsonu <45717030+0xsonu@users.noreply.github.com>
2025-01-27 19:53:28 +0530

    Composit Structure in proto

---
+++ src/main/java/org/sonuk/chapter03/Composition.java
@@ -0,0 +1,35 @@
+package org.sonuk.chapter03;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonuk.models.chapter03.Address;
+import org.sonuk.models.chapter03.School;
+import org.sonuk.models.chapter03.Student;
+
+public class Composition {
+    private static final Logger logger = LoggerFactory.getLogger(Composition.class);
+
+    public static void main(String[] args) {
+        // create student
+        var studentAddress = Address.newBuilder()
+                .setStreet("123, main street")
+                .setCity("Patna")
+                .setState("Bihar")
+                .build();
+
+        var student = Student.newBuilder()
+                .setName("John")
+                .setAddress(studentAddress)
+                .build();
+
+        var school = School.newBuilder()
+                .setId(1)
+                .setName("High School")
+                .setAddress(studentAddress.toBuilder().setStreet("125, main street"))
+                .build();
+
+
+        logger.info(student.toString());
+        logger.info(school.toString());
+    }
+}
