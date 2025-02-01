316fde645baf2a222c5bdb4abc6ac50963e20044
0xsonu <45717030+0xsonu@users.noreply.github.com>
2025-02-02 04:03:31 +0530

    Learned about enum and their default value behaviour

--- src/main/java/org/sonuk/chapter03/MapPractice.java
+++ src/main/java/org/sonuk/chapter03/MapPractice.java
@@ -2,6 +2,7 @@ package org.sonuk.chapter03;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sonuk.model.BodyStyle;
 import org.sonuk.model.Car;
 import org.sonuk.model.Cars;
 import org.sonuk.model.Dealer;
@@ -12,8 +13,15 @@ public class MapPractice {
     private static final Logger log = LoggerFactory.getLogger(MapPractice.class);
 
     public static void main(String[] args) {
-        var car = Car.newBuilder().setMake("Honda").setModel("civic").setYear(2000).build();
-        var car2 = car.toBuilder().setModel("accord").setYear(2000).build();
+        // I learned that default value of enum in proto is the value which is associated
+        // with field id 0;
+        // to demonstrate i am going to create a car with default value
+        var myCar = Car.newBuilder().setMake("Tesla").setModel("Hatchback").build();
+        log.info("Body Style : {}", myCar.getBodyStyle()); // it should print body style as SEDAN because of id 0
+        // but in to string method bodyStyle will not be shown because we have not explicitly set it.
+        log.info("MyCar : {}", myCar);
+        var car = Car.newBuilder().setMake("Honda").setModel("civic").setBodyStyle(BodyStyle.SUV).setYear(2000).build();
+        var car2 = car.toBuilder().setModel("accord").setBodyStyle(BodyStyle.COUPE).setYear(2000).build();
 
         var dealer = Dealer.newBuilder().putInventory(2000, Cars.newBuilder().addAllCar(List.of(car, car2)).build()).build();
         log.info(dealer.toString());
